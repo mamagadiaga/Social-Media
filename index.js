5427e52fd78fb69feb0a81d7e94faeb921deab6a
Mame Gadiaga <mamsogdg@gmail.com>
2023-03-02 17:48:52 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+<script type="text/javascript">
+document.getElementById("toggle").addEventListener("click", function(){
+document.getElementsByTagName('body')[0].classList.toggle("dark-theme")
+});
+</script>
